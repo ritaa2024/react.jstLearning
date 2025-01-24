67c235deaf81b90ce4248ca700a3755f7bec6920
ritaa2024 <ritamahato2001@gmail.com>
2025-01-24 23:09:29 +0530

    upload custom hook,usecallback

--- src/component/ProductList.jsx
+++ src/component/ProductList.jsx
@@ -1,19 +1,24 @@
-import  { useEffect, useState } from "react";
-
+import  {  useState } from "react";
+import { useFetch } from "../../hooks/useFetch";
 function ProductList() {
-  const [products, setProducts] = useState([]);
-console.log(products);
+  // const [products, setProducts] = useState([]);
+ 
 
-  useEffect(() => {
-    fetch("http://localhost:3000/product/")
-      .then(response => response.json())
-      .then(data => setProducts(data));
-  }, []);
+  const [url,setUrl]=useState("http://localhost:3000/product/")
+  
+  const {data:products} = useFetch(url)
+  console.log(products);
+
 
   return (
     <section>
+    <div className="filter">
    
-        {products.map((product)=>(
+   <button onClick={()=>setUrl("http://localhost:3000/product/")}>all</button>
+   <button  onClick={()=>setUrl("http://localhost:3000/product?in_stock=true")}>In stock</button>
+    </div>
+
+        {products && products.map((product)=>(
             <div className="card" key={product.id}>
             <p className="id">{product.id}</p>
             <p className="name">{product.name}</p>
